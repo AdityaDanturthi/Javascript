002342fb84118a406a09fc502b50a55c7e9010d7
Aditya Danturthi <aditya2096@gmail.com>
2022-04-10 21:49:22 -0230

    Add files via upload

--- Passenger Counter App/index.js
+++ Passenger Counter App/index.js
@@ -4,7 +4,12 @@
 // let count = firstBatch + secondBatch;
 // console.log(count)
 
-let myAge = 25;
-let humanDogRatio = 7;
-let myDogAge = myAge * humanDogRatio;
-console.log(myDogAge)
+// let myAge = 25;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge)
+
+// let bonusPoints = 50;
+// bonusPoints = bonusPoints - 25;
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints)
