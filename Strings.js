e384c81d8990e126ba56c7175f06a41171f9901e
Aditya Danturthi <aditya2096@gmail.com>
2022-04-11 22:00:15 -0230

    Add files via upload

--- Strings.js
+++ Strings.js
@@ -1,4 +1,10 @@
-let username = "adi"
-let message = "You have three new notifications"
-console.log(message +"," + username + "!")
-let messageToUser = message +"," + username + "!";
+// let username = "adi"
+// let message = "You have three new notifications"
+// let messageToUser = message +"," + username + "!";
+// console.log(messageToUser)
+
+
+let name = "Aditya"
+let greeting = "Hi my name is "
+let myGreeting = greeting + name
+console.log(myGreeting)
