e27dca7de2218cb24a32f139e659dbb2af67c2c4
Aditya Danturthi <aditya2096@gmail.com>
2022-04-13 21:13:58 -0230

    string concatenation

---
+++ variablesPractice.js
@@ -0,0 +1,7 @@
+// Create two variables, firstName and lastName
+firstName = "Aditya"
+lastName = "Danturthi"
+// Concatenate the two variable into a third variable called fullName
+fullName = firstName+' '+lastName
+// log fullName to the console
+console.log(fullName)
