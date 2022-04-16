21a5eb2db3c9af4b338bfb58c41f8a4f75fc11eb
Aditya Danturthi <aditya2096@gmail.com>
2022-04-15 23:40:27 -0230

    error message variable

---
+++ index.js
@@ -0,0 +1,7 @@
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id = "error"
+let errorEl = document.getElementById("error")
+function errormessage() {
+   errorEl.innerText = "Something went wrong, please try again";
+}
