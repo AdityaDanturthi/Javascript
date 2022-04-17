79939e54d9d6d6ffc5f6c3b5af5e1a4c5e090f6e
Aditya Danturthi <aditya2096@gmail.com>
2022-04-17 21:00:57 -0230

    Add files via upload

--- Calculator/index.js
+++ Calculator/index.js
@@ -3,3 +3,25 @@ let num2 = 2;
 
 document.getElementById("num1-el").textContent = 8;
 document.getElementById("num2-el").textContent = 2;
+
+let addRes = 0
+let subRes = 0
+let mulRes = 0
+let divRes = 0
+
+function add() {
+    addRes = num1 + num2;
+}
+document.getElementById("sum-el").textContent = addRes
+
+function subtract() {
+    subRes = num1 - num2;
+}
+
+function multiply() {
+    mulRes = num1 * num2;
+}
+
+function divide() {
+    divRes = num1 / num2;
+}
